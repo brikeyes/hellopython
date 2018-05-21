5c98e0b8114b8f5027b26e80cfcb6e6b5d5b0126
brian k <bkeyes@vizuri.com>
2018-05-21 16:00:40 -0400

    Update app.py
    due to the fact that the code was looking for an input "what is  your name" I commented out every thing but the first line

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 print("hello world")
-myName = input ("What is your name?")
-myVar = 'Hello'
-print(myName)
-print(myVar)
+#myName = input ("What is your name?")
+#myVar = 'Hello'
+#print(myName)
+#print(myVar)
